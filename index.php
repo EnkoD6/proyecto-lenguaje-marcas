a09b8a5ee39765aa66517e668417b58f3c24cf28
EnekoDev <enekolabdev@gmail.com>
2024-05-15 14:14:06 +0200

    proyecto-dam-lenguaje-marcas

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="course end project">
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href=".\img\uax-logo.webp" type="image/x-icon">
+    <title>Proyecto Intermodular</title>
+</head>
+<body>
+    <header>
+        <h1>Eneko Olabarria</h1>
+        <h2>Desarrollador Web Full Stack</h2>
+    </header>
+    <nav>
+        <a href="#proffesional-exp"><button>Experiencia Profesional</button></a>
+        <a href="#academic-info"><button>Formación Academica</button></a>
+        <a href="#personal-proyects"><button>Proyectos Personales</button></a>
+    </nav>
+    <section class="proffesional-exp" id="proffesional-exp">
+        <h3>Experiencia Profesional</h3>
+        <ul>
+            <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque voluptatum fugit id at error aut eos maiores, corporis illum voluptatem! Minus eos deserunt excepturi iusto veritatis nisi dolore ipsam? Odit.</li>
+            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad tempore eaque amet itaque velit non labore omnis aliquid impedit vero veritatis nisi aliquam accusamus voluptatum explicabo, cupiditate earum ipsam expedita.</li>
+            <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum qui ullam dignissimos vero incidunt ab distinctio, sequi sint at quidem assumenda nisi asperiores, eum quam, aliquam sed quisquam? Provident, deserunt.</li>
+        </ul>
+    </section>
+    <div class="parallax"></div>
+    <section class="academic-info" id="academic-info">
+        <h3>Formación Academica</h3>
+        <ul>
+            <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae totam nisi repellendus culpa amet, magni inventore perferendis exercitationem laudantium provident. Sunt commodi quia adipisci laudantium voluptatum. Eum sunt soluta dolore.</li>
+            <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus distinctio veritatis consequuntur nam laudantium quis laborum temporibus sit animi fugiat id, culpa tenetur exercitationem reprehenderit velit provident enim nihil dolorem?</li>
+            <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed, totam debitis at consequatur repellendus nulla, magnam id dolores illo quam quaerat facere possimus iure, quod dolor ratione tempora voluptates incidunt.</li>
+        </ul>
+    </section>
+    <div class="parallax"></div>
+    <section class="personal-proyects" id="personal-proyects">
+        <h3>Proyectos Personales</h3>
+        <ul>
+            <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae totam nisi repellendus culpa amet, magni inventore perferendis exercitationem laudantium provident. Sunt commodi quia adipisci laudantium voluptatum. Eum sunt soluta dolore.</li>
+            <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus distinctio veritatis consequuntur nam laudantium quis laborum temporibus sit animi fugiat id, culpa tenetur exercitationem reprehenderit velit provident enim nihil dolorem?</li>
+            <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed, totam debitis at consequatur repellendus nulla, magnam id dolores illo quam quaerat facere possimus iure, quod dolor ratione tempora voluptates incidunt.</li>
+        </ul>
+    </section>
+    <footer>
+        <div class="footer-text">
+            <h4>Proyecto desarrollado por Eneko Olabarria para el Grado Desarrollo de Aplicaciones Multiplataforma</h4>
+        </div>
+        <div class="social-networks">
+            <h5>Visita mis redes:</h5>
+            <div class="github-logo"><a href="https://github.com/EnkoD6"><img src=".\img\github-logo.png" alt="github logo" ></a></div> 
+            <div class="linkedin-logo"><a href="https://www.linkedin.com/in/eneko-olabarria-se%C3%B1oran-8b8b8015b/"><img src=".\img\linkedin-logo.webp" alt="linkedin logo"></a></div>
+        </div>
+        </footer>
+        <a href="#"><button class="back-up-button">&#8593;</button></a>
+    </body>
+</html>
